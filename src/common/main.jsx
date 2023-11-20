c4e88257dabb20c5af3fbb66c1e4158b3752fb1b
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-11-19 23:23:19 -0300

    feat(main y scss): se anade bootsrap al proyecto

--- src/common/main.jsx
+++ src/common/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Routing from './Routing.jsx'
+import './scss/styles.scss'
+import * as bootstrap from 'bootstrap'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
