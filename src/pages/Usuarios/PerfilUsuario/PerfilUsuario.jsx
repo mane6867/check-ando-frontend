ffe8800cafc97ef980367c2dc7c60474ab68b3f2
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-11-21 00:34:08 -0300

    tarjeta actividad basica

--- src/pages/Usuarios/PerfilUsuario/PerfilUsuario.jsx
+++ src/pages/Usuarios/PerfilUsuario/PerfilUsuario.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import TarjetaActividad from '../../Actividad/TarjetaActividad/TarjetaActividad';
 
 export default function PerfilUsuario(){
 
     const { nombre } = useParams();
 
+    const actividades = [
+        {id: 1, tiempo: "2", actividad: "Se ha registrado en el checkpoint 4"},
+        {id: 2, tiempo: "30", actividad: "Se ha registrado en el checkpoint 3"},
+        {id: 3, tiempo: "45", actividad: "Se ha registrado en el checkpoint 2"},
+        {id: 4, tiempo: "58", actividad: "Se ha registrado en el checkpoint 1"},
+    ]
+
+    console.log(nombre)
+
     return(
-        <div>Perfil {nombre}</div>
+        <div>
+            <p style={{color: "white"}}>{nombre}</p>
+            {actividades.map((actividad) => (
+                <div key={actividad.id} className="column">
+                    <TarjetaActividad nombre={nombre} tiempo={actividad.tiempo} actividad={actividad.actividad}/>
+                </div>
+            ))}
+
+        </div>
+
     )
 }
