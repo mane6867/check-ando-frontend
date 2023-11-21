8dce377c81885a24b439c8ee3f9627cc13f8505f
mane6867 <magdalena.pino@uc.cl>
2023-11-20 22:07:31 -0300

    base main page

--- src/common/Routing.jsx
+++ src/common/Routing.jsx
@@ -5,9 +5,10 @@ import App from "./App"
 import Navbar from "./Navbar/Navbar"
 import LandingPage from "../pages/LandingPage/LandingPage"
 import './index.css'
-import Estadisticas from "../pages/Estadisticas"
+import Estadisticas from "../pages/Estadísticas/Estadisticas.jsx"
 import Usuarios from "../pages/Usuarios/Usuarios.jsx"
 import Login from "../pages/Login/Login.jsx"
+import MainPage from "../pages/MainPage/MainPage.jsx"
 
 export default function Routing() {
     return(
@@ -20,6 +21,7 @@ export default function Routing() {
             <Route path= "/" element = {<LandingPage/>}></Route>
             <Route path= "/usuarios" element = {<Usuarios/>}></Route>
             <Route path= "/login" element = {<Login/>}></Route>
+            <Route path= "/main-page" element = {<MainPage/>}></Route>
         </Routes>
         
         </BrowserRouter>
