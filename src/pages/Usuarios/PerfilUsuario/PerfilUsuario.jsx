fbf52eb1b0d215b2542bb2d2246791eb1ede5019
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-11-22 12:59:12 -0300

    perfil usuario sin foto

--- src/pages/Usuarios/PerfilUsuario/PerfilUsuario.jsx
+++ src/pages/Usuarios/PerfilUsuario/PerfilUsuario.jsx
@@ -29,7 +29,8 @@ export default function PerfilUsuario(){
             <br></br>
             <div className="row">
                 <div className="col-md-4">
-                    <img src="./../../../assets/mapa.jpeg" alt="Imagen" className="img-fluid rounded-square" />
+                    <div className="div-cuadrado-con-fondo">
+                    </div>
                     <br></br>
                     <button className="btn btn-primary mt-2 btn-success">Editar</button>
                     <br></br>
@@ -43,16 +44,17 @@ export default function PerfilUsuario(){
                     <p style={ pStyle }> {nombre.toLowerCase().split(" ")}@mail.com</p>
                     <p style={ pStyle }>Contacto de emergencia: {<br/>} Claudia Montero</p>
                     <p style={ pStyle }> Celular contacto de emergiencia {<br/>} +56965966038</p>
-                    <h3 class="text-start" style={ { color:"white" } }>Situación actual</h3>
+                    <h1 class="text-start" style={ { color:"white" } }>Situación actual</h1>
                     <p style={ pStyle }>Último checkpoint marcado: 4</p>
                     <p style={ pStyle }>Hora último marcado: 15:34</p>
                     <p style={ pStyle }>Próximo checkpoint esperado: 5</p>
                     <p style={ pStyle }>Hora esperada siguiente marcado: 16:12</p>
+                    <h3 class="text-start" style={ { color:"white" } }>Alerta: Desactivada</h3>
                 </div>
                 <div className="col-md-4">
                     <br></br>
                     <h3 class="text-start" style={ {color: "white"} }>Registro de Actividad</h3>
-                    <div style={ { backgroundColor: "#00471e", height: '70vh', overflow:"hiden" } }>
+                    <div style={ { backgroundColor: "#00471e", height: '70vh', overflow:"scroll" } }>
                         {actividades.map((actividad) => (
                             <div key={actividad.id} className="column">
                                 <TarjetaActividad nombre={nombre} tiempo={actividad.tiempo} actividad={actividad.actividad}/>
