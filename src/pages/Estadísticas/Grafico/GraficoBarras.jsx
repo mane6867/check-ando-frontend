0f95f678d7123b062994a7c74fd6ad72a1124089
Isabel Jofré Birke <isabeljofre@MacBook-Air-de-Isabel.local>
2023-11-23 02:00:58 -0300

    pagina basica fea de estadistica

---
+++ src/pages/Estadísticas/Grafico/GraficoBarras.jsx
@@ -0,0 +1,10 @@
+import './GraficoBarras.css'
+
+export default function GraficoBarras(){
+
+    return(
+    <div className="container-lan" >
+        <div class="img-grafico-barras"></div>
+    </div>
+    )
+}
---
+++ src/pages/Estadísticas/Grafico/GraficoTorta.jsx
@@ -0,0 +1,10 @@
+import './GraficoTorta.css'
+
+export default function GraficoTorta(){
+
+    return(
+    <div className="container-lan" >
+        <div class="img-grafico-torta"></div>
+    </div>
+    )
+}
